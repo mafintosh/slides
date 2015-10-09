9a20c7fe6218a6746cf28435efa360ff23b87fc4
Eduardo Sorribas <eduardo@sorribas.org>
2015-10-09 19:40:38 +0200

    [nodejsconf-it-workshop] Remove unused dependency and fix var name.

--- nodejsconf-it-workshop/code/4/client.js
+++ nodejsconf-it-workshop/code/4/client.js
@@ -1,11 +1,10 @@
-var pumpify = require('pumpify')
 var net = require('net')
 var jsonStream = require('duplex-json-stream')
 
 var username = process.argv[2] || 'guest'
 var hostname = (process.argv[3] || 'anon') + '.local'
 
-var socket = jsonStream(net.connect(10000, serverName))
+var socket = jsonStream(net.connect(10000, hostname))
 
 process.stdin.on('data', function (data) {
   var message = data.toString().trim()
